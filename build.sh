f213d509107fbe42670acd20f493c98c33d8e07a
Matthew Mawdsley <matthew@mawdsley.me>
2021-02-22 20:54:59 +0000

    Add build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+if test -f "keymap.json"; then
+    rm keymap.json
+fi
+
+qmk compile
+qmk c2json -kb preonic/rev3 -km mmawdsley --no-cpp -o keymap.json keymap.c
+python3 layout_generator.py
+rm keymap.json
